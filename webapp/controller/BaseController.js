f0582f038c81d3d28ef7a0ebe4aac7ce68d1f291
Tarento@123## <Jay_Mukherjee@contractor.murphyoilcorp.com>
2022-03-01 12:40:39 +0000

    commented the Contact details
    Change-Id: Ic6f7667ee97e8aa1b06e521272859bc405ba97d0

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -41,7 +41,7 @@ sap.ui.define([
 					"parentDTO": {
 						"customData": {
 							"business_entity": {
-								"entity_type_id": "41001",
+								"entity_type_id": "40002",
 								"created_by": sUserId,
 								"modified_by": sUserId,
 								"is_draft": true
@@ -209,6 +209,7 @@ sap.ui.define([
 			oFilters.countryCode = oCRSearchData.City;
 			oFilters.companyCode = oCRSearchData.CompanyCode;
 			oFilters.entityType = "CUSTOMER";
+			oFilters.entity_type_id = "40002";
 			oFilters.listOfCRSearchCondition = [
 				"GET_CR_BY_ADDRESS",
 				"GET_CR_CREATED_BY_USER_ID",
